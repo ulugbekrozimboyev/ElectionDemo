6c3f36e408e091b1fb6941b2d8bf256d15ad2739
ulugbekrozimboyev <ulugbekrozimboyev@gmail.com>
2021-05-01 15:48:37 +0500

    init project

---
+++ src/main/java/com/example/ElectionDemo/QuestionsList.java
@@ -0,0 +1,35 @@
+package com.example.ElectionDemo;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Author: Ulug'bek Ro'zimboyev  <ulugbekrozimboyev@gmail.com>
+ * Date: 5/1/2021 3:41 PM
+ */
+@WebServlet(name = "QuestionsList", value = "/questions")
+public class QuestionsList extends HttpServlet {
+
+    private String message;
+
+    public void init() {
+        message = "Get all questions";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Salom papalam</title>
+</head>
+<body>
+<h1><%= "Salom papalam!" %>
+</h1>
+<br/>
+<a href="hello-servlet">Hello Servlet</a>
+<a href="questions">Questions</a>
+</body>
+</html>
