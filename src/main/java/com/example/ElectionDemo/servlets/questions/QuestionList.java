294902080a52d2636ba5c1b767a9a35cf447589f
ulugbekrozimboyev <ulugbekrozimboyev@gmail.com>
2021-05-01 17:14:09 +0500

    question :: send params to jsp

--- src/main/java/com/example/ElectionDemo/servlets/questions/QuestionList.java
+++ src/main/java/com/example/ElectionDemo/servlets/questions/QuestionList.java
@@ -1,10 +1,15 @@
 package com.example.ElectionDemo.servlets.questions;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Author: Ulug'bek Ro'zimboyev  <ulugbekrozimboyev@gmail.com>
  * Date: 5/1/2021 3:41 PM
@@ -13,13 +18,22 @@ import java.io.IOException;
 public class QuestionList extends HttpServlet {
 
     private String message;
+    private List<String> questionList;
 
     public void init() {
         message = "Get all questions";
+        questionList = new ArrayList<>();
+        questionList.add(" This is a question one");
+        questionList.add(" This is a question two");
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.sendRedirect("questions/questions.jsp");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        request.setAttribute("message", message);
+        request.setAttribute("questionList", questionList);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("questions/questions.jsp");
+        requestDispatcher.forward(request, response);
     }
 
     public void destroy() {
--- src/main/webapp/questions/questions.jsp
+++ src/main/webapp/questions/questions.jsp
@@ -6,15 +6,18 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
-    <title>Title</title>
+    <title>${message}</title>
 </head>
 <body>
     <h2>Questions</h2>
     <ul>
-        <li>asdas dasd ? </li>
-        <li>sdfsdf ?</li>
+        <c:forEach var="question" items="${questionList}">
+            <li>${question}</li>
+        </c:forEach>
     </ul>
 </body>
 </html>
