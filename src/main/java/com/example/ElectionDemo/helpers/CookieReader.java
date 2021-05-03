8148a4cf0c3c3d5f3f8e3ee94758428e188648a5
Soporboy <saparboy.batirov@gmail.com>
2021-05-04 01:07:57 +0500

    1. Added some useful servlets

---
+++ src/main/java/com/example/ElectionDemo/helpers/CookieReader.java
@@ -0,0 +1,39 @@
+package com.example.ElectionDemo.helpers;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ *
+ * Class which simplifies reading cookies from request.
+ */
+public class CookieReader {
+    private final HttpServletRequest request;
+
+    /**
+     * The constructor.
+     *
+     * @param request request from which cookies will be read
+     */
+    public CookieReader(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    /**
+     * Reads cookie by key from request.
+     *
+     * @param key the key of a cookie
+     * @return returns cookie value
+     */
+    public Optional<String> readCookie(String key) {
+        if (request.getCookies() == null) {
+            return Optional.empty();
+        }
+        return Arrays.stream(request.getCookies())
+                 .filter(c -> key.equals(c.getName()))
+                 .map(Cookie::getValue)
+                 .findAny();
+    }
+}
